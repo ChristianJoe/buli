ba217198363a3ea5f68add2d2b10295f2ed7d883
ChristianJoe <cjoe@gmx.de>
2017-10-07 17:30:57 +0200

    add host and url

--- wahltraud/wahltraud/wahltraud/settings.py
+++ wahltraud/wahltraud/wahltraud/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SITE_URL = 'https://infos.data.wdr.de:8080'
+SITE_URL = 'https://cc8b346a.ngrok.io'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','cc8b346a.ngrok.io' ]
 
 
 # Application definition
