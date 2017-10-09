b8e630167e0e6d470fccc94f82f382e6e5c46284
ChristianJoe <cjoe@gmx.de>
2017-10-09 15:32:14 +0200

    next competition function

--- wahltraud/wahltraud/bot/callbacks/dates.py
+++ wahltraud/wahltraud/bot/callbacks/dates.py
@@ -4,16 +4,15 @@ import operator
 from ..fb import send_buttons, button_postback, send_text, send_attachment, send_list, list_element, quick_reply
 from ..data import by_uuid, get_dates
 import datetime
+import pandas as pd
 
 
 def dates_api(event, parameters, **kwargs):
     sender_id = event['sender']['id']
     club = parameters.get('clubs')
 
-    if not club:
-        send_text(sender_id, 'Der nächste Wettkampf findet am 14.10. statt.')
-    else:
-        next_event(event,{'club':club})
+
+    next_event(event,{'club':club})
 
 
 
@@ -22,15 +21,27 @@ def next_event(event,payload):
     club = payload['club']
 
     dates = get_dates()
-    now = datetime.date.today()
-    for i in range(0, 100):
-        look_up_date = now + datetime.timedelta(days=i)
-        next_dates = dates[dates['date'] == look_up_date]
-        if not next_dates.empty:
-            break
-    next_dates
 
-    send_text(sender_id, 'Der nächste Wettkampftag ist am {date}.'
-              .format(
-                        date = look_up_date
-                    ))
+    now = datetime.date.today()
+
+    if not club:
+        for i in range(0, 100):
+            look_up_date = now + datetime.timedelta(days=i)
+            next_dates = dates[dates['date'] == look_up_date]
+            if not next_dates.empty:
+                break
+
+
+        text =  'Der nächste Wettkampftag ist am {date}.'.format(
+                            date = look_up_date
+                        )
+    else:
+        dates_club = dates[dates['club'] == club]
+        strdate = ''
+        for i in range(0, dates_club.shape[0]):
+            if i > 0:
+                strdate += ' und am '
+            strdate += dates_club['date'].iloc[i].strftime("%d.%m.%Y")
+        text = club + 'ist Ausrichter am ' + strdate + '.'
+
+    send_text(sender_id, text)
