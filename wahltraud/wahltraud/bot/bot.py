27644c3c4521a52a6e98f7e8467f4cbe46a8d3fa
ChristianJoe <cjoe@gmx.de>
2017-10-11 15:24:24 +0200

    result parser

--- wahltraud/wahltraud/bot/bot.py
+++ wahltraud/wahltraud/bot/bot.py
@@ -267,7 +267,7 @@ def dsb_update():
 
 schedule.every(60).seconds.do(push_breaking)
 schedule.every().day.at("18:00").do(push_notification)
-schedule.every().day.at("13:18").do(dsb_update)
+schedule.every().day.at("13:26").do(dsb_update)
 
 
 def schedule_loop():
