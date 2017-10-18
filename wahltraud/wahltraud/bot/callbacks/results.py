58f7c0b2ff6d731ad649fb3cafe902cddec847b1
ChristianJoe <cjoe@gmx.de>
2017-10-18 02:39:21 +0200

    info club

--- wahltraud/wahltraud/bot/callbacks/results.py
+++ wahltraud/wahltraud/bot/callbacks/results.py
@@ -192,10 +192,11 @@ def table_league(event,payload,**kwargs):
         button = button_postback("Andere Liga", {'table_league': {'weapon': 'LG'} })
 
     if offset == 0:
-        send_text(sender_id, '{buli} {weapon}. Gruppe {region}'.format(
+        send_text(sender_id, 'Ich präsentiere die Tabelle der \n\n{buli} {region} - {weapon}\n\n  nach dem {nter} Wettkampftag:'.format(
             buli=buli,
             weapon=weapon,
-            region=region
+            region=region,
+            nter = int((data['team_won']+data['team_lost'])/2)
                     )
                   )
     send_list(sender_id, elements, button=button)
