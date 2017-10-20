eb32ea8fdeee1cea4de27da7403eff0b8b98d16e
ChristianJoe <cjoe@gmx.de>
2017-10-20 16:32:51 +0200

    setlist

--- wahltraud/wahltraud/bot/callbacks/simple.py
+++ wahltraud/wahltraud/bot/callbacks/simple.py
@@ -47,9 +47,9 @@ def club_info(event,payload,**kwargs):
     club = payload['club_info']
 
     club_repl = club
-    for ending in [' II', ' I', ' 2', 'FSG']:
-        if club_repl.endswith(ending):
-            club_repl = club_repl.replace(ending, '').strip()
+    #for ending in [' II', ' I', ' 2', 'FSG']:
+    #    if club_repl.endswith(ending):
+    #        club_repl = club_repl.replace(ending, '').strip()
 
     infoall = get_club_info_weapon_buli_region(club_repl)
 
