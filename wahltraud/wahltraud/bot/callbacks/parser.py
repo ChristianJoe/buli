7f543ac796384fdc0b3bc1af4b9ffdb959cc6a16
ChristianJoe <cjoe@gmx.de>
2017-10-20 17:51:02 +0200

    setlist

--- wahltraud/wahltraud/bot/callbacks/parser.py
+++ wahltraud/wahltraud/bot/callbacks/parser.py
@@ -139,7 +139,7 @@ def get_results_pd():
                             home_team_short = home_team_short.replace(ending, '').strip()
                         if guest_team_short.endswith(ending) or ending == 'FSG':
                             guest_team_short = guest_team_short.replace(ending, '').strip()
-                    if home_team[0] == 'ST Hubertus Elsen:
+                    if home_team[0] == 'ST Hubertus Elsen':
                         home_team[0] = 'ST Hubertus Elsen I'
                     if guest_team[0] == 'ST Hubertus Elsen':
                         guest_team[0] = 'ST Hubertus Elsen I'
