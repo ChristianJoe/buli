bc715cb1f47321150245532b9c588a7d6eb33167
ChristianJoe <cjoe@gmx.de>
2017-10-26 17:10:25 +0200

    result push

--- wahltraud/wahltraud/bot/callbacks/results.py
+++ wahltraud/wahltraud/bot/callbacks/results.py
@@ -1073,7 +1073,7 @@ def buli_live(event,payload=None,**kwargs):
     #links = get_meyton(hrefs = True)
     options = [quick_reply('Aktualisieren', ['buli_live'])]
     if isinstance(payload,pd.DataFrame):
-        live_results = payload
+        live_results = [payload]
     else:
         live_results = get_live_results()
 
