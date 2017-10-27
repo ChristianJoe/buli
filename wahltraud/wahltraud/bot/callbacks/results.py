2b6d31d2f840ec325547e03573a7afc07df95c42
ChristianJoe <cjoe@gmx.de>
2017-10-27 13:30:43 +0200

    result push

--- wahltraud/wahltraud/bot/callbacks/results.py
+++ wahltraud/wahltraud/bot/callbacks/results.py
@@ -1124,8 +1124,6 @@ def buli_live(event,payload=None,**kwargs):
                         guest=live['name'].iloc[(2 * index + 1)],
 
                     )  # ,
-                    print(reply_positions)
-                    send_text(sender_id,reply_positions)
 
                 reply_overview = "{fight}\n{home} : {guest}\n{home_points}:{guest_points}".format(
                             fight =fight,
